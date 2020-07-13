a1eb182d321ef9168dc9c11d532e6d3cb5a2b89f
Alicja-Jot <miedzinska93@gmail.com>
2020-07-13 10:45:11 +0200

    Rozpoczęcie pracy

---
+++ src/main/java/pl/alicjajot/covid/frontend/CovidFrontendApplication.java
@@ -0,0 +1,13 @@
+package pl.alicjajot.covid.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/alicjajot/covid/frontend/CovidFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.alicjajot.covid.frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
